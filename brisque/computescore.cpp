2e071a0fa3905ec3e4ed58e7c3fd56d0f7be6226
wass <wassimbs088@gmail.com>
2024-09-30 06:27:03 +0100

    making it a main repo for BRISQUE

--- brisque/computescore.cpp
+++ brisque/computescore.cpp
@@ -2,7 +2,6 @@
 #include "libsvm/svm.h"
 
 float computescore(cv::Mat frame, struct svm_model* model, int frameNum) {
-  // pre-loaded vectors from allrange file 
   float min_[36] = {0.336999 ,0.019667 ,0.230000 ,-0.125959 ,0.000167 ,0.000616 ,0.231000 ,-0.125873 ,0.000165 ,0.000600 ,0.241000 ,-0.128814 ,0.000179 ,0.000386 ,0.243000 ,-0.133080 ,0.000182 ,0.000421 ,0.436998 ,0.016929 ,0.247000 ,-0.200231 ,0.000104 ,0.000834 ,0.257000 ,-0.200017 ,0.000112 ,0.000876 ,0.257000 ,-0.155072 ,0.000112 ,0.000356 ,0.258000 ,-0.154374 ,0.000117 ,0.000351};
 
   float max_[36] = {9.999411, 0.807472, 1.644021, 0.202917, 0.712384, 0.468672, 1.644021, 0.169548, 0.713132, 0.467896, 1.553016, 0.101368, 0.687324, 0.533087, 1.554016, 0.101000, 0.689177, 0.533133, 3.639918, 0.800955, 1.096995, 0.175286, 0.755547, 0.399270, 1.095995, 0.155928, 0.751488, 0.402398, 1.041992, 0.093209, 0.623516, 0.532925, 1.042992, 0.093714, 0.621958, 0.534484};
@@ -12,18 +11,12 @@ float computescore(cv::Mat frame, struct svm_model* model, int frameNum) {
 
   Mat orig = frame; 
   
-  vector<double> brisqueFeatures; // feature vector initialization
-  ComputeBrisqueFeature(orig, brisqueFeatures); // compute brisque features
+  vector<double> brisqueFeatures; 
+  ComputeBrisqueFeature(orig, brisqueFeatures); 
 
-  // use the pre-trained allmodel file
-
-
-  // float min_[37];
-  // float max_[37];
 
   struct svm_node x[37];
-  // rescale the brisqueFeatures vector from -1 to 1 
-  // also convert vector to svm node array object
+
   for(i = 0; i < 36; ++i) {
     float min = min_[i];
     float max = max_[i];
@@ -36,7 +29,6 @@ float computescore(cv::Mat frame, struct svm_model* model, int frameNum) {
   
   int nr_class=svm_get_nr_class(model);
   double *prob_estimates = (double *) malloc(nr_class*sizeof(double));
-  // predict quality score using libsvm class
   qualityscore = svm_predict_probability(model,x,prob_estimates);
 
   free(prob_estimates);
